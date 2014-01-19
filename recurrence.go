1e05918e0f5825921241657f5d66c1637638a46a
Dan Ott <thedanielott@gmail.com>
2014-01-18 18:14:11 -0800

    First go at temporal expressions

--- recurrence.go
+++ recurrence.go
@@ -1,56 +1,121 @@
 package main
 
-import (
-	"fmt"
-	"time"
+import "time"
+
+const (
+	First  = 1
+	Second = 2
+	Third  = 3
+	Fourth = 4
+	Fifth  = 5
+	Last   = -1
 )
 
 const (
-	Daily   = iota
-	Weekly  = iota
-	Monthly = iota
-	Yearly  = iota
+	Sunday time.Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
 )
 
-type Recurrence struct {
-	Frequency int8
+type TemporalExpression interface {
+	Includes(time.Time) bool
 }
 
-func (r Recurrence) NextDate(d time.Time) (nextDate time.Time) {
-	switch r.Frequency {
-	case Daily:
-		nextDate = d.AddDate(0, 0, 1)
-	case Weekly:
-		nextDate = d.AddDate(0, 0, 7)
-	case Monthly:
-		nextDate = d.AddDate(0, 1, 0)
-	case Yearly:
-		nextDate = d.AddDate(1, 0, 0)
-	}
+type Intersection []TemporalExpression
 
+func (i Intersection) Includes(t time.Time) bool {
+	for _, te := range i {
+		if te.Includes(t) == false {
+			return false
+		}
+	}
+	return true
+}
+
+type Union []TemporalExpression
+
+func (u Union) Includes(t time.Time) bool {
+	for _, te := range u {
+		if te.Includes(t) {
+			return true
+		}
+	}
+	return false
+}
+
+type Difference struct {
+	Include []TemporalExpression
+	Exclude []TemporalExpression
+}
+
+func (d Difference) Includes(t time.Time) bool {
+	for _, te := range d.Exclude {
+		if te.Includes(t) {
+			return false
+		}
+	}
+	for _, te := range d.Include {
+		if te.Includes(t) {
+			return true
+		}
+	}
+	return false
+}
+
+type DateRange struct {
+	Start time.Time
+	End   time.Time
+}
+
+func (d DateRange) Includes(t time.Time) bool {
+	return !(t.Before(d.Start) || t.After(d.End))
+}
+
+func (d DateRange) EachDate() (result []time.Time) {
+	for t := d.Start; t.Before(d.End); t = t.AddDate(0, 0, 1) {
+		result = append(result, t)
+	}
 	return
 }
 
-func (r Recurrence) GenerateDays(d time.Time, end time.Time) chan time.Time {
-	c := make(chan time.Time)
+type WeekdayTE struct {
+	Weekday time.Weekday
+}
 
-	go func() {
-		for d.Before(end) {
-			d = r.NextDate(d)
-			c <- d
-		}
-		close(c)
-	}()
+func (d WeekdayTE) Includes(t time.Time) bool {
+	return t.Weekday() == d.Weekday
+}
 
-	return c
+type WeekdayInMonthTE struct {
+	Week    int
+	Weekday time.Weekday
+}
+
+func (d WeekdayInMonthTE) Includes(t time.Time) (r bool) {
+	if d.Week > 0 {
+		r = (d.Weekday == t.Weekday() && weekFromMonthStart(t) == d.Week)
+	} else {
+		r = (d.Weekday == t.Weekday() && weekFromMonthEnd(t) == d.Week)
+	}
+	return
+}
+
+func weekFromMonthStart(t time.Time) int {
+	return ((t.Day() - 1) / 7) + 1
+}
+
+func daysIn(m time.Month, year int) int {
+	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
+}
+
+func weekFromMonthEnd(t time.Time) int {
+	d := daysIn(t.Month(), t.Year())
+	return (((d - t.Day()) / 7) * -1) - 1
 }
 
 func main() {
-	rd := Recurrence{Daily}
-	start := time.Now().Truncate(time.Hour)
-	end := time.Now().Truncate(time.Hour).AddDate(2, 0, 0)
-
-	for d := range rd.GenerateDays(start, end) {
-		fmt.Println(d)
-	}
 }
