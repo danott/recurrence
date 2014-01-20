4c603fe13238d28784d997c4a96769093cc05cb2
Dan Ott <thedanielott@gmail.com>
2014-01-19 20:28:06 -0800

    Set operations test

---
+++ set_operations_test.go
@@ -0,0 +1,32 @@
+package recurrence
+
+import "testing"
+
+func TestUnion(t *testing.T) {
+	i := Union{OrdinalWeekday{First, Sunday}, Day{Last}}
+	r := YearRange(2006)
+
+	assertOnlyIncludes(t, r, i, "2006-01-01", "2006-02-05", "2006-03-05",
+		"2006-04-02", "2006-05-07", "2006-06-04", "2006-07-02", "2006-08-06",
+		"2006-09-03", "2006-10-01", "2006-11-05", "2006-12-03", "2006-01-31",
+		"2006-02-28", "2006-03-31", "2006-04-30", "2006-05-31", "2006-06-30",
+		"2006-07-31", "2006-08-31", "2006-09-30", "2006-10-31", "2006-11-30",
+		"2006-12-31")
+}
+
+func TestIntersection(t *testing.T) {
+	i := Intersection{Month{January}, Weekday{Sunday}}
+	r := YearRange(2006)
+
+	assertOnlyIncludes(t, r, i, "2006-01-01", "2006-01-08", "2006-01-15",
+		"2006-01-22", "2006-01-29")
+}
+
+func TestDifference(t *testing.T) {
+	d := Difference{[]Rule{Day{Last}}, []Rule{Month{November}}}
+	r := YearRange(2006)
+
+	assertOnlyIncludes(t, r, d, "2006-01-31", "2006-02-28", "2006-03-31",
+		"2006-04-30", "2006-05-31", "2006-06-30", "2006-07-31", "2006-08-31",
+		"2006-09-30", "2006-10-31", "2006-12-31")
+}
