9d49c118e69aa43d9261f7786df7588819cbc7f3
Dan Ott <thedanielott@gmail.com>
2014-01-19 19:31:11 -0800

    Dry up time_range_test

--- time_range_test.go
+++ time_range_test.go
@@ -5,10 +5,10 @@ import (
 	"time"
 )
 
-func TestYearRange(t *testing.T) {
-	r := YearRange(2006)
-	expectedStartTime, _ := time.Parse(f, "2006-01-01")
-	expectedEndTime, _ := time.Parse(f, "2006-12-31")
+func assertStartTimeAndEndTime(t *testing.T, r TimeRange, startTime string, endTime string) {
+	expectedStartTime, _ := time.Parse(f, startTime)
+	expectedEndTime, _ := time.Parse(f, endTime)
+
 	if !r.startTime.Equal(expectedStartTime) {
 		t.Errorf("%s should have been %s", r.startTime, expectedStartTime)
 	}
@@ -18,15 +18,13 @@ func TestYearRange(t *testing.T) {
 	}
 }
 
-func TestMonthRange(t *testing.T) {
-	r := MonthRange(time.January, 2006)
-	expectedStartTime, _ := time.Parse(f, "2006-01-01")
-	expectedEndTime, _ := time.Parse(f, "2006-01-31")
-	if !r.startTime.Equal(expectedStartTime) {
-		t.Errorf("%s should have been %s", r.startTime, expectedStartTime)
-	}
+func TestYearRange(t *testing.T) {
+	assertStartTimeAndEndTime(t, YearRange(2006), "2006-01-01", "2006-12-31")
+	assertStartTimeAndEndTime(t, YearRange(2014), "2014-01-01", "2014-12-31")
+}
 
-	if !r.endTime.Equal(expectedEndTime) {
-		t.Errorf("%s should have been %s", r.endTime, expectedEndTime)
-	}
+func TestMonthRange(t *testing.T) {
+	assertStartTimeAndEndTime(t, MonthRange(time.January, 2006), "2006-01-01", "2006-01-31")
+	assertStartTimeAndEndTime(t, MonthRange(time.February, 2006), "2006-02-01", "2006-02-28")
+	assertStartTimeAndEndTime(t, MonthRange(time.February, 2008), "2008-02-01", "2008-02-29")
 }
