1c96bae6868b96b1a6846c2e50db5059c86d7f2b
Dan Ott <thedanielott@gmail.com>
2014-01-19 20:51:24 -0800

    Generate Dates from a TimeRange

--- time_range.go
+++ time_range.go
@@ -11,13 +11,6 @@ func (r TimeRange) Includes(t time.Time) bool {
 	return !(t.Before(r.startTime) || t.After(r.endTime))
 }
 
-func (r TimeRange) eachDate() (result []time.Time) {
-	for t := r.startTime; t.Before(r.endTime); t = t.AddDate(0, 0, 1) {
-		result = append(result, t)
-	}
-	return
-}
-
 func YearRange(y int) TimeRange {
 	startTime := time.Date(y, time.January, 1, 0, 0, 0, 0, time.UTC)
 	endTime := time.Date(y+1, time.January, 0, 0, 0, 0, 0, time.UTC)
@@ -28,4 +21,20 @@ func MonthRange(m time.Month, y int) TimeRange {
 	startTime := time.Date(y, m, 1, 0, 0, 0, 0, time.UTC)
 	endTime := time.Date(y, m+1, 0, 0, 0, 0, 0, time.UTC)
 	return TimeRange{startTime, endTime}
+}
+
+func Dates(t TimeRange, r Rule) (result []time.Time) {
+	for _, t := range t.eachDate() {
+		if r.Includes(t) {
+			result = append(result, t)
+		}
+	}
+	return
+}
+
+func (r TimeRange) eachDate() (result []time.Time) {
+	for t := r.startTime; t.Before(r.endTime); t = t.AddDate(0, 0, 1) {
+		result = append(result, t)
+	}
+	return
 }
