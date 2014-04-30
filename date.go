49aa2f4e7df9ed1439026b9907fff7c46ba70f04
Dan Ott <thedanielott@gmail.com>
2014-04-30 15:21:48 -0700

    Docs

--- date.go
+++ date.go
@@ -5,12 +5,15 @@ import (
 	"time"
 )
 
+// Date is a specific day. Shorthand for Intersection{Year, Month, Day}.
 type Date time.Time
 
+// Implement Schedule interface.
 func (self Date) IsOccurring(t time.Time) bool {
 	return self.asIntersection().IsOccurring(t)
 }
 
+// Implement Schedule interface.
 func (self Date) Occurrences(t TimeRange) chan time.Time {
 	return self.asIntersection().Occurrences(t)
 }
@@ -23,6 +26,7 @@ func (self Date) asIntersection() Intersection {
 	}
 }
 
+// Implement json.Unmarshaler interface.
 func (self *Date) UnmarshalJSON(b []byte) error {
 	t, err := time.Parse(`"2006-01-02"`, string(b))
 
@@ -34,12 +38,14 @@ func (self *Date) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// Implement json.Marshaler interface.
 func (self Date) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
 		"date": time.Time(self).Format("2006-01-02"),
 	})
 }
 
+// NewDate let's you create a new Date from the time format "2006-01-02"
 func NewDate(s string) Date {
 	t, err := time.Parse("2006-01-02", s)
 
