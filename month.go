0e760c092be392790a7f710186c74401d235949a
Dan Ott <thedanielott@gmail.com>
2014-05-01 15:25:27 -0700

    Month using NextAfter

--- month.go
+++ month.go
@@ -28,8 +28,36 @@ func (self Month) IsOccurring(t time.Time) bool {
 	return t.Month() == time.Month(self)
 }
 
-func (self Month) Occurrences(t TimeRange) chan time.Time {
-	return t.occurrencesOfSchedule(self)
+func (self Month) Occurrences(tr TimeRange) chan time.Time {
+	ch := make(chan time.Time)
+
+	go func() {
+		start := tr.Start.AddDate(0, 0, -1)
+		end := tr.End
+		for t, err := self.NextAfter(start); err == nil && !t.After(end); t, err = self.NextAfter(t) {
+			if !t.After(end) {
+				ch <- t
+			}
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+func (self Month) NextAfter(t time.Time) (time.Time, error) {
+	desiredMonth := int(self)
+	tMonth := int(t.Month())
+
+	if tMonth < desiredMonth {
+		return time.Date(t.Year(), time.Month(desiredMonth), 1, 0, 0, 0, 0, time.UTC), nil
+	}
+
+	if tMonth > desiredMonth || (tMonth == desiredMonth && isLastDayInMonth(t)) {
+		return time.Date(t.Year()+1, time.Month(desiredMonth), 1, 0, 0, 0, 0, time.UTC), nil
+	}
+
+	return t.AddDate(0, 0, 1), nil
 }
 
 func (self Month) MarshalJSON() ([]byte, error) {
@@ -67,4 +95,8 @@ func (self *Month) UnmarshalJSON(b []byte) error {
 	}
 
 	return nil
+}
+
+func (self Month) String() string {
+	return time.Month(self).String()
 }
